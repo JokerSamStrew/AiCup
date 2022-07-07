8bf7d1da464cb055b54a4f6e0456a502c1f0a166
Simon <s.labzoff@yandex.ru>
2022-07-07 22:52:07 +0300

    fix scripts

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,2 +1,7 @@
 cmake --build ../aicup22/out
-../app/aicup22 --antialias false --config ../aicup22/tests/app_configs/auto_run_config.json
+../app/aicup22 --antialias false --config ../aicup22/tests/app_configs/auto_run_config.json &
+aicup22=$!
+sleep 2
+../aicup22/out/ai_cup_22 &
+ai_cup_22=$!
+wait $aicup22 $ai_cup_22
--- scripts/build_and_run_batch_mode.sh
+++ scripts/build_and_run_batch_mode.sh
@@ -3,5 +3,11 @@ mkdir -p ../temp/local
 ../app/aicup22 --antialias false --config ../aicup22/tests/app_configs/auto_run_config.json \
     --batch-mode \
     --save-replay ../temp/local/replay.rep \
-    --save-results ../temp/local/resulsts.txt
+    --save-results ../temp/local/resulsts.txt &
+aicup22=$!
+sleep 2
+../aicup22/out/ai_cup_22 &
+ai_cup_22=$!
+wait $aicup22 $ai_cup_22
+
 ../app/aicup22 --antialias false --replay ../temp/local/replay.rep
