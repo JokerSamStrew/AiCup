f2fe78ea16166f170735a3d5bb063ff2074a1107
Simon <s.labzoff@yandex.ru>
2022-07-10 17:16:12 +0300

    debug info

--- aicup22/aicup22/MyStrategy.cpp
+++ aicup22/aicup22/MyStrategy.cpp
@@ -1,5 +1,6 @@
 #include "MyStrategy.hpp"
 #include <exception>
+#include "debugging/Color.hpp"
 
 MyStrategy::MyStrategy(const model::Constants &constants) {}
 
@@ -13,19 +14,32 @@ model::Order MyStrategy::getOrder(const model::Game &game, DebugInterface *debug
     std::unordered_map<int, model::UnitOrder> actions;
     for (auto &unit : game.units)
     {
+        auto currentDirection = model::Vec2(0, 0);
         if (unit.playerId != game.myId)
+        {
+            debugInterface->addCircle(unit.position, 2, debugging::Color(0.0, 0.5, 0.0, 0.5));
+            currentDirection = model::Vec2(unit.position.x, unit.position.y);
             continue;
+        } 
+        else
+        {
+            currentDirection = model::Vec2(-unit.direction.y, unit.direction.x);
+        }
+            
 
         std::shared_ptr<model::ActionOrder::Aim> aim = std::make_shared<model::ActionOrder::Aim>(true);
         std::optional<std::shared_ptr<model::ActionOrder>> action = std::make_optional(aim);
         auto currentCenterVec = getNextZoneCenterVec(game, unit);
 
-        model::UnitOrder order(currentCenterVec, {-unit.direction.y, unit.direction.x}, action);
+        model::UnitOrder order(currentCenterVec, currentDirection, action);
         actions.insert({unit.id, order});
     }
     return model::Order(actions);
 }
 
-void MyStrategy::debugUpdate(DebugInterface &debugInterface) {}
+void MyStrategy::debugUpdate(DebugInterface &debugInterface) 
+{
+    debugInterface.flush();
+}
 
 void MyStrategy::finish() {}
