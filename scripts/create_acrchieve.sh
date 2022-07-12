ee612dcc4e318a640d8f10afb95c6e994d66a69f
Simon <s.labzoff@yandex.ru>
2022-07-12 14:02:00 +0300

    create archieve script fix

--- scripts/create_acrchieve.sh
+++ scripts/create_acrchieve.sh
@@ -1,11 +1,3 @@
-rm ../temp/aicup22.zip
-find ../aicup22/aicup22 -type f -name "*.*pp" \
-    ! -path "../aicup22/aicup22/codegame/*" \
-    ! -path "../aicup22/aicup22/debugging/*" \
-    ! -path "../aicup22/aicup22/model/*" \
-    ! -name "CMakeLists.*" \
-    ! -name "DebugInterface.*" \
-    ! -name "Stream.*" \
-    ! -name "TcpStream.*" \
-    ! -name "main.*" \
-    -print | zip -r ../temp/aicup22.zip -@
+cd ../aicup22/aicup22
+rm ../../temp/aicup22.zip
+zip -r ../../temp/aicup22.zip .
