46a8cc93da344f6dd0faae8ee7baa068fa3a5701
Simon <s.labzoff@yandex.ru>
2022-07-14 12:42:54 +0300

    v1.2.2

--- aicup22/aicup22/main.cpp
+++ aicup22/aicup22/main.cpp
@@ -6,9 +6,10 @@
 #include <memory>
 #include <string>
 
-class Runner {
+class Runner
+{
 public:
-    Runner(const std::string& host, int port, const std::string& token): tcpStream(host, port)
+    Runner(const std::string &host, int port, const std::string &token) : tcpStream(host, port)
     {
         tcpStream.write(token);
         tcpStream.write(int(1));
@@ -20,21 +21,33 @@ public:
     {
         DebugInterface debugInterface(&tcpStream);
         std::shared_ptr<MyStrategy> myStrategy = std::shared_ptr<MyStrategy>();
-        while (true) {
-            auto message = codegame::ServerMessage::readFrom(tcpStream);
-            if (auto updateConstantsMessage = std::dynamic_pointer_cast<codegame::ServerMessage::UpdateConstants>(message)) {
+        while (true)
+        {
+            auto message = codegame::readServerMessage(tcpStream);
+            if (const codegame::UpdateConstants *updateConstantsMessage = std::get_if<codegame::UpdateConstants>(&message))
+            {
                 myStrategy.reset(new MyStrategy(updateConstantsMessage->constants));
-            } else if (auto getOrderMessage = std::dynamic_pointer_cast<codegame::ServerMessage::GetOrder>(message)) {
-                codegame::ClientMessage::OrderMessage(myStrategy->getOrder(getOrderMessage->playerView, getOrderMessage->debugAvailable ? &debugInterface : nullptr)).writeTo(tcpStream);
+            }
+            else if (const codegame::GetOrder *getOrderMessage = std::get_if<codegame::GetOrder>(&message))
+            {
+                codegame::ClientMessage message = codegame::OrderMessage(myStrategy->getOrder(getOrderMessage->playerView, getOrderMessage->debugAvailable ? &debugInterface : nullptr));
+                codegame::writeClientMessage(message, tcpStream);
                 tcpStream.flush();
-            } else if (auto finishMessage = std::dynamic_pointer_cast<codegame::ServerMessage::Finish>(message)) {
+            }
+            else if (const codegame::Finish *finishMessage = std::get_if<codegame::Finish>(&message))
+            {
                 myStrategy->finish();
                 break;
-            } else if (auto debugUpdateMessage = std::dynamic_pointer_cast<codegame::ServerMessage::DebugUpdate>(message)) {
+            }
+            else if (const codegame::DebugUpdate *debugUpdateMessage = std::get_if<codegame::DebugUpdate>(&message))
+            {
                 myStrategy->debugUpdate(debugUpdateMessage->displayedTick, debugInterface);
-                codegame::ClientMessage::DebugUpdateDone().writeTo(tcpStream);
+                codegame::ClientMessage message = codegame::DebugUpdateDone();
+                codegame::writeClientMessage(message, tcpStream);
                 tcpStream.flush();
-            } else {
+            }
+            else
+            {
                 throw std::runtime_error("Unexpected server message");
             }
         }
@@ -44,7 +57,7 @@ private:
     TcpStream tcpStream;
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     std::string host = argc < 2 ? "127.0.0.1" : argv[1];
     int port = argc < 3 ? 31001 : atoi(argv[2]);
